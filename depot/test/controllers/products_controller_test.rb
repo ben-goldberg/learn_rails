50f44566cf5d333b56a0eac14aac456faa17e44a
ben-goldberg <bgoldberg15@cmc.edu>
2015-08-16 12:38:35 -0700

    added controller tests

--- depot/test/controllers/products_controller_test.rb
+++ depot/test/controllers/products_controller_test.rb
@@ -52,4 +52,10 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
+  test "should get sidebar" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4
+  end
 end
