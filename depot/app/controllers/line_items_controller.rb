b61036bd10901d356f04cf4e4b2a82d2afca8c89
ben-goldberg <bgoldberg15@cmc.edu>
2015-08-27 16:43:59 -0700

    ready (kinda) for deployment

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
+  skip_before_action :authorize, only: :create
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: :create
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -27,11 +28,12 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create 
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
+        format.html { redirect_to store_url }
+        format.js {@current_item = @line_item}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -60,9 +62,11 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @line_item = LineItem.find(params[:id])
     @line_item.destroy
+
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to store_path }
       format.json { head :no_content }
     end
   end
@@ -75,6 +79,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
